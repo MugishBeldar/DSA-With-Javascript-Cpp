3ec9025a140763980c39c22644b519b52abf626d
MugishBeldar <mugishbeldar333@gmail.com>
2024-11-15 10:42:56 +0530

    Two sum problem

---
+++ Javascript/004 two sum problem/index.js
@@ -0,0 +1,20 @@
+// Ques 4 - Two Sum
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1] (Because nums[0] + nums[1] == 9, we return [0, 1])
+// Input: nums = [3, 2, 4], target = 6
+// Output: [1, 2]
+
+let twoSum = (arr, sum) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === sum) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 13));  // Output: [0, 1]
