ebeb1287159afcb07bbbfc2578fd00debb13fc88
MugishBeldar <mugishbeldar333@gmail.com>
2024-11-17 13:26:49 +0530

    Bubble sort

---
+++ Javascript/#9 Sorting Algorithm/#1 Bubble sort/index.js
@@ -0,0 +1,21 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(j, j + 1, array);
+      }
+    }
+  }
+  return array;
+}
+
+function swap(i, j, array) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+console.log(bubbleSort([11, 333, 1, 5, 3, 2, 0, 1]));
+
+// Time Complexity = O(n^2)
+// Space Complexity = O(1)
